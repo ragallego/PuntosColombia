209621111de785aaa45d5d562dd489ee1cadc211
Roberto Andres Gallego Loaiza <rogallego@personalsoft.com>
2018-05-22 10:55:38 -0500

    Se carga el proyecto al repositorio

---
+++ PuntosColombia/PuntosColombia.Excepciones/OrdeNumerico.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntosColombia.Excepciones
+{
+    public static class OrdeNumerico
+    {
+        public static int[] missingNumbers(int[] arr, int[] brr, out int diff)
+        {
+            try
+            {
+                //Group the values and add the Count field as the number of times repeated in the array
+                var array1 = arr.GroupBy(c => c)
+                             .Select(group => new
+                             {
+                                 Value = group.Key,
+                                 Count = group.Count()
+                             }).ToList();
+
+                var array2 = brr.GroupBy(c => c)
+                             .Select(group => new
+                             {
+                                 Value = group.Key,
+                                 Count = group.Count()
+                             }).ToList();
+
+                //Produces the set difference of two sequences by using the default equality comparer to compare values
+                var result = array1.Except(array2).ToList();
+                var result2 = array2.Except(array1).ToList();
+
+                //Add the result of the two arrays
+                var result3 = result.Concat(result2).Select(t => t.Value).Distinct().ToList();
+
+                //The difference between maximum and minimum number in the second list is less than or equal to 100
+                diff = array2.Max(k => k.Value) - array2.Min(k => k.Value);
+
+                return result3.Select(k => k).OrderBy(k => k).ToArray();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ PuntosColombia/PuntosColombia/Program.cs
@@ -0,0 +1,127 @@
+﻿using PuntosColombia.Excepciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntosColombia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables para el proceso general
+            int diff = 0;
+            int[] arr;
+            int[] brr;
+            int resultValue = 0;
+            int n = 0;
+            int m = 0;
+
+            try
+            {
+                //Validacion de ingreso de tamaño del primer vector
+                Console.WriteLine("Tamaño del primer Vector");
+                int.TryParse(Console.ReadLine(), out resultValue);
+
+                if (resultValue != 0)
+                {
+                    n = resultValue;
+                    resultValue = 0;
+                }
+                else
+                    Close("El dato ingresado debe ser númerico, presione ENTER para salir");
+
+                //Validacion de ingreso de tamaño del segundo vector
+                Console.WriteLine("Tamaño del segundo vector");
+                int.TryParse(Console.ReadLine(), out resultValue);
+
+                if (resultValue != 0)
+                {
+                    m = resultValue;
+                    resultValue = 0;
+                }
+                else
+                    Close("El dato ingresado debe ser númerico, presione ENTER para salir");
+
+                // Constraint: n<=m
+                if (m <= n)
+                    Close("El tamaño del segundo arreglo debe ser mayor al del primer arreglo, presione ENTER para salir");
+
+                // Constraint: 1<=n,m<=2*100000
+                if ((n < 1 || n >= 200000) || (m < 1 || m >= 200000))
+                    Close("El tamaño de los arreglos deben estar entre 1 y 200000, presione ENTER para salir");
+
+                //Ingreso de los valores del primer vector
+                Console.WriteLine("Ingresa los valores del primer arreglo, separados por comas");
+                var val = Console.ReadLine().Split(',');
+                arr = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    // Constraint: x intersecion B
+                    int tamano = val.Length;
+                    if (tamano == n)
+                    {
+                        int.TryParse(val[i], out resultValue);
+
+                        if (resultValue != 0)
+                            arr[i] = resultValue;
+                        else
+                            Close("Los valores deben ser numéricos, presione ENTER para salir");
+                    }
+                    else
+                        Close("El tamaño del arreglo no puede ser mayor al definido inicialmente, presione ENTER para salir");
+
+                    resultValue = 0;
+                }
+
+                //Ingreso de los valores del segundo vector
+                Console.WriteLine("Ingresa los valores del segundo arreglo, separados por comas");
+                var val2 = Console.ReadLine().Split(',');
+                brr = new int[m];
+                for (int i = 0; i < m; i++)
+                {
+                    // Constraint: x intersecion B
+                    int tamano = val.Length;
+                    if (tamano == n)
+                    {
+                        int.TryParse(val2[i], out resultValue);
+
+                        if (resultValue != 0)
+                            brr[i] = resultValue;
+                        else
+                            Close("Los valores deben ser numéricos, presione ENTER para salir");
+                    }
+                    else
+                        Close("El tamaño del arreglo no puede ser mayor al definido inicialmente, presione ENTER para salir");
+
+                    resultValue = 0;
+                }
+
+                // Constraint: 1<=x <=10000
+                if ((arr.Where(k => k <= 1 || k >= 10000).Count() > 0) || (brr.Where(k => k <= 1 || k >= 10000).Count() > 0))
+                    Close("Los valores de los arreglos deben estar entre 1 y 10000, presione ENTER para salir");
+
+                int[] result = OrdeNumerico.missingNumbers(arr, brr, out diff);
+
+                // Constraint: Xmax - Xmin < 101
+                if (diff > 100)
+                    Close("La diferencia entre el valor máximo y minimo del segundo arreglo no debe ser menor o igual a 100, presione ENTER para salir");
+
+                Close(string.Concat("Los números faltantes son: ", String.Join(" ", result)));
+            }
+            catch (Exception excepcion)
+            {
+                Close("El sistema lanzó la siguiente excepción: " + excepcion.Message + " presione ENTER para salir");
+            }
+        }
+
+        public static void Close(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
